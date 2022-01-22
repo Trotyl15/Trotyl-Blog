21756264ab6c64a6901e752ccec970d8cb6ed0fa
trotyl15 <mail@trotyl.xyz>
2022-01-21 19:05:39 -0500

    Site updated: 2022-01-21 19:05:37

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -4,12 +4,12 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         $('[rel="icon"]').attr('href');
-        document.title = '╭(°A°`)╮这就走了？';
+        document.title = '(|||ﾟдﾟ)这就走了？';
         clearTimeout(titleTime);
     }
     else {
         $('[rel="icon"]').attr('href');
-        document.title = '(ฅ>ω<*ฅ)欢迎回来｜' + OriginTitle;
+        document.title = '(`ヮ´ )欢迎回来｜' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
         }, 2000);
